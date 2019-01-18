bbafae40633e807f1a82e44a2195198ecb0eb276
Han Le <vanhan91@gmail.com>
2019-01-18 12:00:51 +0700

    fix bug

--- popup.js
+++ popup.js
@@ -37,20 +37,21 @@ chrome.contextMenus.removeAll(function(){
 	chrome.contextMenus.create(collinsItem);
 	chrome.contextMenus.create(merriamItem);
 	chrome.contextMenus.create(longmanItem);
-	chrome.contextMenus.onClicked.addListener(function (clickData){
-		if (clickData.menuItemId == 'lookupCambridge' && clickData.selectionText) {
-			var myWindow = window.open("https://dictionary.cambridge.org/dictionary/english/"+clickData.selectionText, "", "width=500,height=500");
-		}
-		if (clickData.menuItemId == 'lookupCollins' && clickData.selectionText) {
-			var myWindow = window.open("https://www.collinsdictionary.com/dictionary/english/"+clickData.selectionText, "", "width=500,height=500");
-		}
-		if (clickData.menuItemId == 'lookupMerriamWebster' && clickData.selectionText) {
-			var myWindow = window.open("https://www.merriam-webster.com/dictionary/"+clickData.selectionText, "", "width=500,height=500");
-		}
-		if (clickData.menuItemId == 'lookupLongman' && clickData.selectionText) {
-			var myWindow = window.open("https://www.ldoceonline.com/dictionary/"+clickData.selectionText, "", "width=500,height=500");
-		}
-	});
+});
+
+chrome.contextMenus.onClicked.addListener(function (clickData){
+	if (clickData.menuItemId == 'lookupCambridge' && clickData.selectionText) {
+		var myWindow = window.open("https://dictionary.cambridge.org/dictionary/english/"+clickData.selectionText, "", "width=500,height=500");
+	}
+	if (clickData.menuItemId == 'lookupCollins' && clickData.selectionText) {
+		var myWindow = window.open("https://www.collinsdictionary.com/dictionary/english/"+clickData.selectionText, "", "width=500,height=500");
+	}
+	if (clickData.menuItemId == 'lookupMerriamWebster' && clickData.selectionText) {
+		var myWindow = window.open("https://www.merriam-webster.com/dictionary/"+clickData.selectionText, "", "width=500,height=500");
+	}
+	if (clickData.menuItemId == 'lookupLongman' && clickData.selectionText) {
+		var myWindow = window.open("https://www.ldoceonline.com/dictionary/"+clickData.selectionText, "", "width=500,height=500");
+	}
 });
 
 // chrome.commands.onCommand.addListener(function(command) {
